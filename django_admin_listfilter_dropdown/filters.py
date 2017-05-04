04305c6a2996aadd10dfee11cac48fc182c619fb
Ihor Polyakov <ihoru@users.noreply.github.com>
2017-05-04 17:29:44 +0700

    Added ChoiceDropdownFilter
    ChoiceDropdownFilter is needed with not related fields, but those which have choices field

--- django_admin_listfilter_dropdown/filters.py
+++ django_admin_listfilter_dropdown/filters.py
@@ -1,6 +1,9 @@
-from django.contrib.admin.filters import AllValuesFieldListFilter, RelatedFieldListFilter
+from django.contrib.admin.filters import AllValuesFieldListFilter, RelatedFieldListFilter, ChoicesFieldListFilter
 
 class DropdownFilter(AllValuesFieldListFilter):
+    template = 'django_admin_listfilter_dropdown/dropdown_filter.html'
+
+class ChoiceDropdownFilter(ChoicesFieldListFilter):
     template = 'django_admin_listfilter_dropdown/dropdown_filter.html'
 
 class RelatedDropdownFilter(RelatedFieldListFilter):
